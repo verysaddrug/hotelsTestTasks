1ded2c5247281bd2effab0cb11960a8eb3145e13
verysaddrug <147279913+verysaddrug@users.noreply.github.com>
2024-05-15 00:47:28 +0300

    Update task4.go

--- task4.go
+++ task4.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+)
+
+// Функция для вывода таблицы умножения
+func printMultiplicationTable(n int) {
+	// Используем вложенные циклы для генерации строк и столбцов таблицы умножения
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= n; j++ {
+			fmt.Printf("%4d", i*j)
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	var n int
+	fmt.Println("Введите число для генерации таблицы умножения:")
+	fmt.Scanln(&n)
+
+	printMultiplicationTable(n)
+}
