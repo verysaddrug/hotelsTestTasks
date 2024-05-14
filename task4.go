af9391cbe4b77db7d9973b6853ae4badb1baf004
verysaddrug <147279913+verysaddrug@users.noreply.github.com>
2024-05-15 00:47:54 +0300

    Update task4.go

--- task4.go
+++ task4.go
@@ -1,24 +1,39 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
 )
 
 // Функция для вывода таблицы умножения
 func printMultiplicationTable(n int) {
-	// Используем вложенные циклы для генерации строк и столбцов таблицы умножения
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= n; j++ {
-			fmt.Printf("%4d", i*j)
-		}
-		fmt.Println()
-	}
+    // Выводим заголовок таблицы
+    fmt.Printf("    ")
+    for i := 1; i <= n; i++ {
+        fmt.Printf("%4d", i)
+    }
+    fmt.Println()
+
+    // Выводим разделительную линию
+    fmt.Printf("    ")
+    for i := 1; i <= n; i++ {
+        fmt.Printf("----")
+    }
+    fmt.Println()
+
+    // Выводим строки таблицы умножения
+    for i := 1; i <= n; i++ {
+        fmt.Printf("%4d|", i) // Выводим номер строки
+        for j := 1; j <= n; j++ {
+            fmt.Printf("%4d", i*j)
+        }
+        fmt.Println()
+    }
 }
 
 func main() {
-	var n int
-	fmt.Println("Введите число для генерации таблицы умножения:")
-	fmt.Scanln(&n)
-
-	printMultiplicationTable(n)
+    var n int
+    fmt.Println("Введите число для генерации таблицы умножения:")
+    fmt.Scanln(&n)
+    
+    printMultiplicationTable(n)
 }
